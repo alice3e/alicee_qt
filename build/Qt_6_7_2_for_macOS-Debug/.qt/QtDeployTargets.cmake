217954a863912cba0f779689a698cd9f305cebdd
alicee <avega2003@yandex.ru>
2024-09-20 23:47:06 +0300

    refactoring, .gitignore changes

--- build/Qt_6_7_2_for_macOS-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_alicee_qt_FILE /Users/alicee/Desktop/Work_Main/alicee_qt/build/Qt_6_7_2_for_macOS-Debug/alicee_qt.app/Contents/MacOS/alicee_qt)
-set(__QT_DEPLOY_TARGET_alicee_qt_TYPE EXECUTABLE)
